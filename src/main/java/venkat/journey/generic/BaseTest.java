2c3769d2864e4d0244e32b33e8258839086d53bd
venkat <venkatmanganapalli9014@gmail.com>
2018-01-29 15:22:09 +0530

    nothing

--- src/main/java/venkat/journey/generic/BaseTest.java
+++ src/main/java/venkat/journey/generic/BaseTest.java
@@ -2,12 +2,8 @@ package venkat.journey.generic;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
